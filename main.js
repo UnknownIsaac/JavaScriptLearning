d60013411c4d9ce5a13217a49be57ee34409c2f8
Isaac Sims <unknownisaac@MacBook-Air-de-Isaac.local>
2023-03-29 17:44:31 +0200

    added: reverse string program

--- main.js
+++ main.js
@@ -4,26 +4,36 @@ let Pet = {
     isOld : true,
     firstName : undefined,
     furcolor : "red",
+    sound : function(){
+        let noice = "meow";
+        return noice
+    },
+    family : [
+        {name: "Bootsy"},
+        {name: "Sims"},
+        {name: "Moon"},
+        
+    ],
 };
+
+// console.log(Pet["family"][2]["name"])
+
 
 // Dot notation
 Pet.name = 'dog';
 
-console.log(Pet.name);
+// console.log(Pet.name);
 // Bracket notation
 let selection = 'age';
 Pet[selection];
 
-console.log(Pet[selection])
-
-
 // Arrays
 
-let selectedColor = ['red', 'blue'];
-selectedColor[2] = 0
-console.log(selectedColor);
-console.log(selectedColor[0])
-console.log(selectedColor[1])
+// let selectedColor = ['red', 'blue'];
+// selectedColor[2] = 0
+// console.log(selectedColor);
+// console.log(selectedColor[0])
+// console.log(selectedColor[1])
 
 // function
 
@@ -31,4 +41,24 @@ function greet(hey){
     console.log('hey ' + hey)
 }
 
-greet('bye pussy')
+// greet('bye pussy')
+// console.log(Pet.family[2]);
+// 
+// console.log(Pet["name"])
+
+let car = {
+    make: "Corvette",
+    speed: 160,
+    engine: {
+        size: 2.0,
+        make: "bmw",
+        fuel: "petrol",
+        pistons: [
+            { maker: "BMW"},
+            { maker: "BMW2"},
+        ],
+    },
+    drive: function(){return "drive";}
+};
+
+// console.log(car["engine"]["pistons"][1]["maker"])
