dd495d2913052dbdb3da8d5ce34d7ed69f2a9c0f
NikaVBB <isaacsims100@gmail.com>
2023-04-08 21:39:52 +0200

    d

--- reverse.js
+++ reverse.js
@@ -13,4 +13,49 @@ let joinArray = stringArray.join("");
 return joinArray
 }
 
-console.log(reverseString("hello"))
+console.log(reverseString("hello"));
+
+const lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const lista2 = new Array(2, "Hola", true, 100);
+const lista3 = new Array(10);
+lista3[5] = "Hola" 
+console.log(lista1);
+console.log(lista2);
+console.log(lista3);
+lista3[5] = "Holacaracola";
+console.log(lista3);
+const movil = {
+    altura : 10,
+    anchura : 5,
+    marca : "Samsung",
+    isWhite : true,
+    contactos : ["Juan", "Pedro", "Maria"],
+    tarjeta : {
+        marca : "Nvidia",
+        almacenamietno : "128GB"
+    },
+    "altura-tarjeta" : 10,
+}
+
+movil.anyo = 2019;
+movil.marca = "Apple";
+console.log(movil.altura);
+console.log(movil.marca);
+
+// fechas
+const ahora = new Date();
+console.log(ahora);
+
+const fecha_milis = new Date(10000000000); // utiliza milisegundos
+console.log(fecha_milis);
+
+const fecha_cadena = new Date("December 11 2020");
+console.log(fecha_cadena);
+
+const fecha_valores = new Date(2022, 0, 12);
+console.log(fecha_valores);
+
+const dia = ahora.getDate();
+const mes = ahora.getMonth();
+const anyo = ahora.getFullYear();
+console.log(dia, mes, anyo);
---
+++ task2.js
@@ -0,0 +1,10 @@
+const name = 'Isaac';
+const age = 20;
+let eresDesarrollador = true;
+const fechaNacimiento = new Date(2002, 4, 21);
+let libroFavorito = {
+    titulo : 'La Hija de la noche',
+    autor : 'Laura Gallego García',
+    fecha : new Date(2004),
+    url : 'https://www.lauragallego.com/libros/la-hija-de-la-noche/'
+};
