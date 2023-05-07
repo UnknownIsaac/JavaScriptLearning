ff6237bdacdd112ce3a8c55a59cccba2ea40e44b
Isaac Sims <unknownisaac@MacBook-Air-de-Isaac.local>
2023-05-07 15:48:14 +0200

    d

--- sesiones/sesion-05-Numbers/exam.js
+++ sesiones/sesion-05-Numbers/exam.js
@@ -1,5 +1,14 @@
 let heightcm = 190
-let heightmt = 1.91;
+let heightmt = 1.9;
 let weightkg = 73.5;
-let heightmtroundedUp = heightmt.toFixed(0);
-let weightkgroundedDown = weightkg.toFixed(0);
+let heightmtroundedUp = Math.ceil(heightmt);
+let weightkgroundedDown = Math.floor(weightkg);
+console.log(heightmtroundedUp);
+console.log(weightkgroundedDown);
+let biggestJSNumber = function() {
+    if (Number.MAX_VALUE) {
+        console.log("El número más grande de JavaScript es " + Number.MAX_VALUE);
+    }
+};
+
+biggestJSNumber();
